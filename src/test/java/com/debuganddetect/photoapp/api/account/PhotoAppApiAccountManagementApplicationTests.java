15a029006da7a3eebb4729ef964c700fae1ce6a3
kunal kumar <kunal.soft.in@gmail.com>
2022-06-29 23:26:56 +0530

    Initial commit

---
+++ src/test/java/com/debuganddetect/photoapp/api/account/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debuganddetect.photoapp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
